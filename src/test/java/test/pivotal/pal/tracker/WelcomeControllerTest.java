b3714ae328c8ce90ffffc9e98d5e6d16547de31a
= <=>
2020-02-25 08:59:34 -0700

    Updated test

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -3,9 +3,9 @@ package test.pivotal.pal.tracker;
 import io.pivotal.pal.tracker.HelloService;
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 
@@ -15,6 +15,8 @@ public class WelcomeControllerTest {
     @Test
     public void itSaysHello() throws Exception {
         HelloService helloService = mock(HelloService.class);
+        given(helloService.getMessage()).willReturn("Hello from test");
+
         WelcomeController controller = new WelcomeController(helloService);
 
         assertThat(controller.sayHello()).isEqualTo("Hello from test");
